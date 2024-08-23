a111af7bcf0c4a0050fbe692b606fb2b94bfc31c
Rana Rashid <rrashid.physics@gmail.com>
2024-08-23 11:50:51 +0500

    assignment complete

---
+++ app.js
@@ -0,0 +1,68 @@
+// Q1
+// alert("Hello Sir");
+
+// Q2
+// alert("Error! Please enter a valid password");
+// Q3
+// alert("Welcome to JS Land...\nHappyCoding");
+
+//q4
+// alert("Welcome to JS Land...");
+
+// alert("Happy Coding!\nPrevent This page from creating additional dialogs.");
+
+//q5
+// alert("Hello... I can run JS through my web browser's console");
+// alert("Hello Sir");
+
+// Chapter 2
+
+//q1
+// var username;
+// username = " Rana Rashid";
+
+//q2
+// var myName;
+// myName = "Rana Rashid Abdul Aleem";
+
+//q3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+//q4
+// var studentName;
+// var age;
+// var completionMessage;
+
+// studentName="John Doe";
+// age = "15 years old";
+// completionMessage = "Certified Mobile Application Development";
+
+// alert(studentName);
+// alert(age);
+// alert(completionMessage);
+
+//q5
+// var message="PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(message)
+
+//q6
+// var email;
+// email="rrashid.physics@gmail.com";
+// alert("My email address is "+ email)
+
+//q7
+var book;
+book = "I am trying to learn from the Book A smarter\nway to learn Javascript.";
+alert(book);
+
+//q8
+var message;
+message = "Yah ! I can write HTML content through JavaScript";
+alert(message);
+
+//q9
+var message;
+message = "---------symbol---------";
+alert(message);
